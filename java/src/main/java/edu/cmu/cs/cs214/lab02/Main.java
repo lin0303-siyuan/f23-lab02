89c1c226afcdca7821b2d7620221224935c0afbb
linsiyuan0303 <slin3@andrew.cmu.edu>
2023-09-06 13:17:25 -0400

    fix: modify rectangle to be Shape type in Java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -2,9 +2,11 @@ package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(3, 3);
+        Shape rectangle = new Rectangle(3, 3);
 
         Renderer renderer = new Renderer(rectangle);
 
